381aabb899440875df263f746828a35c2fd7618b
Valdemar <gravemaggot1@gmail.com>
2020-04-20 23:13:08 +0300

    real C

--- app.rb
+++ app.rb
@@ -46,7 +46,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello, Vovanus! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello, Vovanus! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://vk.com/gravemaggot/\">Sir</a>"			
 end
 
 get '/about' do
